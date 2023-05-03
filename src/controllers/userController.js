403904cd853d13eb669fb7a9a4d59144df45dd9b
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-05-02 23:03:20 -0300

    feat: add token and validator

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,75 @@
 const prisma = require('../databases')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const validator = require('validator')
+
+
+const registerUser = async(req, res) => {
+    const {email, password, name} = req.body
+
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({msg: "Email inválido"})
+    }
+
+    if (!validator.isStrongPassword(password, {minLength: 8})) {
+        return res.status(400).json({msg: "Senha inválido"})
+    }
+
+    if (!validator.isAlpha(name, 'pt-BR', {ignore: ' '})) {
+        return res.status(400).json({msg: "Nome inválido. Deve conter apenas letras e espaços;"})
+    }
+
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password: passwordHash,
+            name
+        }
+    })
+
+    res.status(201).json({msg: "Usuário criado"})
+}
+ 
+const login = async (req, res) => {
+
+    const {email, password} = req.body
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email: email
+        }
+    })
+
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        return res.status(404).json({msg: "Senha inválida"})
+    }
+
+    try {
+ 
+        const secret = process.env.SECRET
+        const token = jwt.sign({
+            id: user.id,
+            userId: user.id
+        }, secret)
+
+        const decoded = jwt.verify(token, secret)
+        const userId = decoded.id
+
+        res.status(200).json({msg : "Autenticação realizada", token, userId})
+
+    } catch(err) {
+        console.log(err);
+    }
+}
 
 const getAllUsers = async (req, res) => {
     const user = await prisma.user.findMany()
     return res.status(200).json(user)
-}
-
-const newUser = async (req, res) => {
-    const { body } = req
-    const user = await prisma.user.create({
-        data: {
-            ...body
-        }
-    })
-
-    return res.status(201).json(user)
 }
 
 const getAllProducts = async (req, res) => {
@@ -35,18 +91,32 @@ const getProductId = async (req, res) => {
 
 const newProduct = async (req, res) => {
     const { body } = req
-    const product = await prisma.product.create({
-        data: {
-            ...body,
-            author: {
-                connect: {
-                    id: 2
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    try {
+        const secret = process.env.SECRET
+        const decoded = jwt.verify(token, secret)
+        const userId = decoded.id
+
+        const product = await prisma.product.create({
+            data: {
+                ...body,
+                author: {
+                    connect: {
+                        id: userId
+                    }
                 }
             }
-        }
-    })
+        })
 
-    return res.status(201).json(product)
+        return res.status(201).json(product)
+
+    } catch(error) {
+        console.log(error)
+        return res.status(500).json({msg: "Erro ao criar o produto"})
+    }
+    
 }
 
 const deleteProduct = async (req, res) => {
@@ -62,9 +132,10 @@ const deleteProduct = async (req, res) => {
 
 module.exports = {
     getAllUsers,
-    newUser,
     newProduct,
     getAllProducts,
     getProductId,
-    deleteProduct
+    deleteProduct,
+    registerUser,
+    login
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,14 +1,16 @@
 const Router = require("express");
 const userController = require('../controllers/userController')
+const checkToken = require('../middleware/checkToken')
 
 const router = new Router()
 
-router.get('/user', userController.getAllUsers)
-router.post('/user/create', userController.newUser)
-router.get('/product', userController.getAllProducts)
-router.get('/product/:authorId', userController.getProductId)
-router.post('/product/create', userController.newProduct)
-router.delete('/product/remove/:id', userController.deleteProduct)
+router.get('/user', userController.getAllUsers) // Lista todos os users
+router.post('/user/register', userController.registerUser) // Registra um novo user
+router.post('/user/login', userController.login) // Faz login no usuário
+router.get('/product', userController.getAllProducts) // Lista todos os produtos
+router.get('/product/:authorId', userController.getProductId) // Lista produtos pelo id passado
+router.post('/product/create', checkToken, userController.newProduct) // Cria um novo produto
+router.delete('/product/remove/:id', userController.deleteProduct) // Deleta um produto
 
 module.exports = router
 
