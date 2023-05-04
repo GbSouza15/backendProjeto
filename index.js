2743b21070d8a985654cd1f4f5625576fb39076f
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-05-04 08:48:41 -0300

    feat: update URL for PORT

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(userRoutes)
 app.use(cors)
 
-const PORT = process.env.URL || 5000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is runing or port: ${PORT}`);
