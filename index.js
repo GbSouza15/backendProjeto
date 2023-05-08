11618fabdfce34ba3a456dded3710be9d22e9781
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-05-08 20:25:53 -0300

    feat: update cors

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const express = require('express')
 const userRoutes = require('./src/routes/userRouter')
 const cors = require('cors')
 
-const corsOptions = {
-    origin: '*'
-  };
-
 const app = express()
 app.use(express.json())
+app.use(cors());
 app.use(userRoutes)
 app.use('/files', express.static('uploads'))
-app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3030
 
 app.listen(PORT, () => {
