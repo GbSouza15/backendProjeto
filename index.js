faf46e034fe72f6bdaf11db8a44ba88448fbc1b3
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-05-08 21:26:51 -0300

    feat: add path

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const userRoutes = require('./src/routes/userRouter')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 app.use(express.json())
 app.use(cors());
-app.use('/files', express.static(__dirname + "/uploads"))
+app.use('/files', express.static(path.join(__dirname, 'uploads')));
 app.use(userRoutes)
 const PORT = process.env.PORT || 3030
 
