6a92001746645dd0f6e6fc95ee5f292be524202d
Gabriel <gabriel.souza2004@outlook.com.br>
2023-06-04 14:27:27 -0300

    feat: update new version

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 app.use(cors());
+
 app.use('/files', express.static(path.join(__dirname, 'uploads')));
 app.use(userRoutes)
 const PORT = process.env.PORT || 3030
